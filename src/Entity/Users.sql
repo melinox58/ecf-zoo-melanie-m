9f325141709cd66c1e8597604cc19d9b0fd60f9d
Martinon <melinox89@yahoo.fr>
2024-10-15 15:23:39 +0200

    start of sql

---
+++ src/Entity/Users.sql
@@ -0,0 +1,9 @@
+INSERT INTO Users (firstName, name, role, password, email, isVerified)
+    VALUE (
+        "José",
+        "Martinon",
+        [\"ROLE_ADMIN\"],
+        "$vU9o4JNRxksgclc7XltUduZInECHQFqGPnHkHELt7GXaYbyQisgaK",
+        "zoo.arcadia.martinon@gmail.com",
+        true
+    )
