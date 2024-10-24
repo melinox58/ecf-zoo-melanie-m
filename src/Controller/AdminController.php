88908cc22b1ec5ec08ec9acc0381ad7791481e76
Martinon <melinox89@yahoo.fr>
2024-10-24 10:43:16 +0200

    secure admin space

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -11,8 +11,16 @@ class AdminController extends AbstractController
     #[Route('/admin', name: 'app_admin')]
     public function index(): Response
     {
+        // Vérifier si l'utilisateur a le rôle d'administrateur
+        if (!$this->isGranted('ROLE_ADMIN')) {
+            // Rediriger vers la page d'accueil si l'utilisateur n'est pas administrateur
+            return $this->redirectToRoute('app_home');
+        }
+
+        // Rendre la vue pour l'administrateur
         return $this->render('admin/index.html.twig', [
             'controller_name' => 'AdminController',
         ]);
     }
 }
+
