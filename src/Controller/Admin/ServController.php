9ea7da9dedff124ed4e8afe52890d237173c7e94
Martinon <melinox89@yahoo.fr>
2024-10-25 16:04:24 +0200

    add list services on dashboard admin

---
+++ src/Controller/Admin/ServController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use App\Repository\ServicesRepository;
+
+class ServController extends AbstractController
+{
+    #[Route('/admin/serv', name: 'app_admin_serv')]
+    public function index(ServicesRepository $servicesRepository): Response
+    {
+        $services = $servicesRepository->findBy([], ['name' =>
+        'asc']);
+
+        return $this->render('admin/services/index.html.twig', compact
+        ('services'));
+    }
+}
+
