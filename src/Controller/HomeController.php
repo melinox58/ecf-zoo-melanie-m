82fc215efc811c3890a1be2ae5cc24d4ddbf3103
Martinon <melinox89@yahoo.fr>
2024-11-06 08:53:55 +0100

    insert schedules in home page ok

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -7,6 +7,7 @@ use Symfony\Component\Routing\Attribute\Route;
 use App\Service\MongoDBService;
 use MongoDB\BSON\ObjectId;
 
+use function Symfony\Component\DependencyInjection\Loader\Configurator\iterator;
 
 class HomeController extends AbstractController
 {
@@ -20,14 +21,17 @@ class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        // Utilisez $this->mongoDBService pour accéder au service
-        $collection = $this->mongoDBService->getCollection('opinions');
+        // Récupération des avis validés, triés par date décroissante
+        $opinionsCollection = $this->mongoDBService->getCollection('opinions');
+        $opinions = iterator_to_array($opinionsCollection->find(['isValidated' => true], ['sort' => ['date' => -1]]));
 
-        // Ne récupérer que les avis validés
-        $opinions = iterator_to_array($collection->find(['isValidated' => true], ['sort' => ['date' => -1]]));
+        // Récupération des horaires (ajustez le filtre si besoin)
+        $schedulesCollection = $this->mongoDBService->getCollection('schedules');
+        $schedules = iterator_to_array($schedulesCollection->find());
 
         return $this->render('home/index.html.twig', [
             'opinions' => $opinions,
+            'schedules' => $schedules,
         ]);
     }
 }
