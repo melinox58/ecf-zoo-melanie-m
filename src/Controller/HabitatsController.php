693edc878341991bf43661207eb999a1f20d9f70
Martinon <melinox89@yahoo.fr>
2024-11-12 11:03:01 +0100

    add image in card habitats

--- src/Controller/HabitatsController.php
+++ src/Controller/HabitatsController.php
@@ -2,17 +2,40 @@
 
 namespace App\Controller;
 
+use App\Entity\Animals;
+use App\Entity\Habitats;
+use App\Service\PictureService;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
 class HabitatsController extends AbstractController
 {
+    private EntityManagerInterface $entityManager;
+    private PictureService $pictureService;
+
+    // Injection de l'EntityManager et du service PictureService dans le constructeur
+    public function __construct(EntityManagerInterface $entityManager, PictureService $pictureService)
+    {
+        $this->entityManager = $entityManager;
+        $this->pictureService = $pictureService;
+    }
+
     #[Route('/habitats', name: 'app_habitats')]
     public function index(): Response
     {
+        // On récupère les animaux par habitat spécifique
+        $jungleAnimals = $this->entityManager->getRepository(Animals::class)->findBy(['idHabitats' => 1]);
+        $maraisAnimals = $this->entityManager->getRepository(Animals::class)->findBy(['idHabitats' => 2]);
+        $savaneAnimals = $this->entityManager->getRepository(Animals::class)->findBy(['idHabitats' => 3]);
+
+        // Envoie des données à la vue
         return $this->render('habitats/index.html.twig', [
-            'controller_name' => 'HabitatsController',
+            'jungleAnimals' => $jungleAnimals,
+            'maraisAnimals' => $maraisAnimals,
+            'savaneAnimals' => $savaneAnimals,
+            'pictureService' => $this->pictureService,
         ]);
     }
 }
