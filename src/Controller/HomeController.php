819f2e758f81986cb9030002817f7d5401bb9752
melinox58 <melinox89@yahoo.fr>
2025-01-10 09:00:40 +0100

    add favorites animals in home page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,7 +1,10 @@
 <?php
 namespace App\Controller;
 
+use App\Entity\Animals;
+use App\Entity\Reports;
 use App\Repository\HabitatsRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -18,10 +21,11 @@ class HomeController extends AbstractController
     }
     
     #[Route('/', name: 'app_home')]
-    public function index(HabitatsRepository $habitatsRepository): Response
+    public function index(HabitatsRepository $habitatsRepository, EntityManagerInterface $em): Response
     {
         // Récupérer les habitats
         $habitats = $habitatsRepository->findAllById();
+        $topAnimals = $em->getRepository(Animals::class)->findBy([], ['views' => 'DESC'], 4);
 
         // Récupérer les avis et horaires comme avant
         $opinionsCollection = $this->mongoDBService->getCollection('opinions');
@@ -30,10 +34,18 @@ class HomeController extends AbstractController
         $schedulesCollection = $this->mongoDBService->getCollection('schedules');
         $schedules = iterator_to_array($schedulesCollection->find());
 
+        // Préparer un tableau associatif pour lier chaque animal à ses rapports
+        $reportsByAnimal = [];
+        foreach ($topAnimals as $animal) {
+            $reportsByAnimal[$animal->getId()] = $em->getRepository(Reports::class)->findBy(['idAnimals' => $animal->getId()], ['date' => 'DESC']);
+        }
+
         return $this->render('home/index.html.twig', [
             'opinions' => $opinions,
             'schedules' => $schedules,
             'habitats' => $habitats,  // Passez aussi les habitats à la vue
+            'topAnimals' => $topAnimals,
+            'reportsByAnimal' => $reportsByAnimal,
         ]);
     }
 }
